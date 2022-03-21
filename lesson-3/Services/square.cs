02569b2b65e12d0e18efc3f36dc069ae131b6a89
orugam1 <86592742+gfdhshfs@users.noreply.github.com>
2022-03-21 09:31:04 +0300

    Add files via upload

---
+++ lesson-3/Services/square.cs
@@ -0,0 +1,33 @@
+﻿namespace Services
+{
+    public class square;
+    {
+        public static string Square(double a, double b, double c) 
+    {
+        double d = (b * b) - (4 * a* c);
+
+        if (d > 0)
+        {
+            double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+            double x1 = (-b - Math.Sqrt(d)) / (2 * a);
+            return $"Корень x1 = {x1}, x2 = {x2}";
+        }
+
+        else if (d == 0)
+        {
+            double x1 = -b / (2 * a);
+            return $"Корень x ={x1}";
+        }
+        else
+        {
+            return "Корни отсутствуют";
+        }
+    }
+    
+}
+     
+
+    
+
+
+
